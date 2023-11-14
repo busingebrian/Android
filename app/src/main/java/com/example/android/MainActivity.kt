98e6f3395c40ea9bfd173c1511dc2f9fe9ef87da
Businge Brian <busingebrian@gmail.com>
2023-11-14 18:55:47 +0300

    themes

---
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var buttonGoTo : Button
+    private lateinit var nameTextView : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonGoTo = findViewById(R.id.button_go_to)
+        nameTextView = findViewById(R.id.nameTextView)
+
+        buttonGoTo.setOnClickListener {
+            Intent(this@MainActivity, SecondActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+}
