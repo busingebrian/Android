b0115d54c85228e2a6ea935bedf67880a34d4ce9
Businge Brian <busingebrian@gmail.com>
2023-11-19 01:18:46 +0300

    scrollview

--- app/src/main/java/com/example/android/RelativeLayout.kt
+++ app/src/main/java/com/example/android/RelativeLayout.kt
@@ -1,11 +1,21 @@
 package com.example.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RelativeLayout : AppCompatActivity() {
+    private lateinit var loginButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_layout)
+        loginButton = findViewById(R.id.relative_layout_button)
+
+        loginButton.setOnClickListener {
+            Intent(this@RelativeLayout, NestedScrollView::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
