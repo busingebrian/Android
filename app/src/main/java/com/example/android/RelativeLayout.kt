9e8945699d3339ad8ef307378d17ebafb17d7e73
Businge Brian <busingebrian@gmail.com>
2023-11-19 02:54:30 +0300

    Constraight Layout

--- app/src/main/java/com/example/android/RelativeLayout.kt
+++ app/src/main/java/com/example/android/RelativeLayout.kt
@@ -6,16 +6,24 @@ import android.os.Bundle
 import android.widget.Button
 
 class RelativeLayout : AppCompatActivity() {
-    private lateinit var loginButton : Button
+    private lateinit var loginButton: Button
+    private lateinit var signUpCLButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_layout)
         loginButton = findViewById(R.id.relative_layout_button)
+        signUpCLButton = findViewById(R.id.relative_layout_signup)
 
         loginButton.setOnClickListener {
             Intent(this@RelativeLayout, NestedScrollView::class.java).also {
                 startActivity(it)
             }
         }
+signUpCLButton.setOnClickListener {
+    Intent(this@RelativeLayout, ConstraintLayout::class.java).also {
+        startActivity(it)
+    }
+}
+
     }
 }
